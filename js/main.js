e921f0687ce4173c8bfa257851211669c249637d
Julia Apreleva <12630m@mail.ru>
2020-06-21 22:41:56 +0300

    Загрузка данных о похожих объявлениях с сервера

--- js/main.js
+++ js/main.js
@@ -22,17 +22,13 @@
     }
   };
 
-  // Функция обработки ошибок при загрузке данных с сервера
+  // Функция обработки ошибок при загрузке данных с сервера,
+  // через добавление сообщения в имеющийся в разметке шаблон/template
   var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
+    var template = document.querySelector('#error');
+    var message = template.content.querySelector('.error__message');
+    message.textContent = errorMessage;
+    document.body.appendChild(template.content.cloneNode(true));
   };
 
   // Функция активации страницы
